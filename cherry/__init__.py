ffa10e73c3af56a63551b840dd47692da65eae53
Kraken5651 <karan5651k@example.com>
2026-05-17 20:22:11 +0530

    Progress

--- cherry/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Cherry AI personal assistant."""
-
-__version__ = "1.0.0"
--- cherry/musicLibrary.py
+++
@@ -1,6 +0,0 @@
-music = {
-    "love" : "https://www.youtube.com/watch?v=cswfR85D7jM&list=RDcswfR85D7jM&start_radio=1&pp=ygULbG92ZSBtZSBub3SgBwE%3D",
-    "loser" : "https://www.youtube.com/watch?v=s3a4OQR-10M&list=RDs3a4OQR-10M&start_radio=1&pp=0oAcB",
-    "hill" : "https://www.youtube.com/watch?v=wp43OdtAAkM&list=RDwp43OdtAAkM&start_radio=1&pp=oAcB0gcJCTwKAYcqIYzv"
-    
-}
